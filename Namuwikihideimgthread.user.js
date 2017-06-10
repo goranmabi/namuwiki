20ade4a4d4ccf7b26e7c605cf2081035252786b3
goranmabi <qwell33@naver.com>
2017-06-10 21:24:13 +0900

    firstcommit

---
+++ Namuwikihideimgthread.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name          Namuwiki hide img discuss thread
+// @description	  Hide hidden discuss thread in namuwiki.
+// @author        mabi
+// @include       https://namu.wiki/*
+// @run-at        document-end
+// @version       0.2
+// ==/UserScript==
+(function() {
+$('img').each(function( index ) {
+	$(this).parent().parent().remove();
+});
+})();
