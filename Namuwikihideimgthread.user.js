ec19b9f43a06d1005c815b65f7199d61d4db6656
goranmabi <qwell33@naver.com>
2017-06-10 21:35:26 +0900

    errreport

--- Namuwikihideimgthread.user.js
+++ Namuwikihideimgthread.user.js
@@ -2,7 +2,7 @@
 // @name          Namuwiki hide img discuss thread
 // @description	  Hide hidden discuss thread in namuwiki.
 // @author        mabi
-// @include       https://namu.wiki/*
+// @include       https://namu.wiki/topic/*
 // @run-at        document-end
 // @version       0.2
 // ==/UserScript==
